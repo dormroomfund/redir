b247d74ce3fc35aa53fcfbce5dd84c0771d0fa72
Yasyf Mohamedali <yasyfm@gmail.com>
2017-09-15 02:29:58 -0700

    Fix a bunch of bugs.

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
-from flask import Flask, request, redirect
+import os
+from flask import Flask, redirect
 from collections import defaultdict
 
 SERVER_NAME = 'drf.vc'
-REDIRS = defaultdict(lambda: 'https://{}'.format(SERVER_NAME), {
+REDIRS = defaultdict(lambda: 'https://www.{}'.format(SERVER_NAME), {
   'join': 'https://medium.com/@dormroomfund/join-the-dorm-room-fund-team-9a01f8326401',
   'join-eng': 'https://medium.com/@yasyf/come-hack-at-dorm-room-fund-4a38bbe8354c'
 })
 
 app = Flask(__name__)
-app.config['SERVER_NAME'] = SERVER_NAME
+app.config['DEBUG'] = bool(os.getenv('DEBUG'))
+app.config['SERVER_NAME'] = '{}:80'.format(SERVER_NAME)
 
-@app.route('/')
-def redir():
-  subdomain = request.host[:-len(SERVER_NAME)].rstrip('.')
+@app.route('/', subdomain="<subdomain>")
+def redir(subdomain):
   return redirect(REDIRS[subdomain], code=301)
