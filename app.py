3f9b6ffe6641948e055dcd1f8722a4d3d3d48a68
Yasyf Mohamedali <yasyfm@gmail.com>
2017-10-09 15:52:44 -0700

    Add 5years.

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ from collections import defaultdict
 SERVER_NAME = 'drf.vc'
 REDIRS = defaultdict(lambda: 'https://www.{}'.format(SERVER_NAME), {
   'join': 'https://medium.com/@dormroomfund/join-the-dorm-room-fund-team-9a01f8326401',
-  'join-eng': 'https://medium.com/@yasyf/come-hack-at-dorm-room-fund-4a38bbe8354c'
+  'join-eng': 'https://medium.com/@yasyf/come-hack-at-dorm-room-fund-4a38bbe8354c',
+  '5years': 'https://5years.dormroomfund.com',
 })
 
 app = Flask(__name__)
