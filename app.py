badbd4af306284fca7e6410d81e40faf18993a94
Vinai Rachakonda <rachakondavinai@gmail.com>
2019-08-25 22:12:58 -0400

    Update join.drf

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from collections import defaultdict
 
 SERVER_NAME = 'drf.vc'
 REDIRS = defaultdict(lambda: 'https://www.{}'.format(SERVER_NAME), {
-  'join': 'https://medium.com/@dormroomfund/join-the-dorm-room-fund-team-9a01f8326401',
+  'join': 'https://medium.com/@dormroomfund/join-the-dorm-room-fund-team-2019-e3bf9be34f73',
   'join-eng': 'https://medium.com/@yasyf/come-hack-at-dorm-room-fund-4a38bbe8354c',
   '5years': 'https://5years.dormroomfund.com',
   'femalefounders':'https://femalefoundersdrf.squarespace.com/',
